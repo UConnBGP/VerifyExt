a426bca2b8ce3fef922663dc80f380a7309b5005
Iuconnu <matzerath@protonmail.com>
2019-10-22 13:51:14 -0400

    Plot for levenshtein distance

--- statistics.py
+++ statistics.py
@@ -129,24 +129,17 @@ def load_data(trial, fn):
             s.num_to_method(line_n%11, trial, row)
             line_n += 1
 
-def plot_ld(data):
-    N = 5
-    menMeans = (20, 35, 30, 35, 27)
-    womenMeans = (25, 32, 34, 20, 25)
-    menStd = (2, 3, 4, 1, 2)
-    womenStd = (3, 5, 2, 3, 3)
+def plot_ld(f, oo, np):
+    N = len(data)
     ind = np.arange(N)    # the x locations for the groups
-    width = 0.35       # the width of the bars: can also be len(x) sequence
 
-    p1 = plt.bar(ind, menMeans, width, yerr=menStd)
-    p2 = plt.bar(ind, womenMeans, width,
-                         bottom=menMeans, yerr=womenStd)
+    p1 = plt.scatter(f, ind, label="Full")
+    p1 = plt.scatter(oo, ind, label="Origin Only")
+    p1 = plt.scatter(np, ind, label="MRT No Propagation")
 
-    plt.ylabel('Scores')
-    plt.title('Scores by group and gender')
-    plt.xticks(ind, ('G1', 'G2', 'G3', 'G4', 'G5'))
-    plt.yticks(np.arange(0, 81, 10))
-    plt.legend((p1[0], p2[0]), ('Men', 'Women'))
+    plt.ylabel('32 Collectors')
+    plt.title('Levenshtein Distance')
+    plt.legend(()
 
     plt.show()
 
@@ -181,10 +174,16 @@ def main():
     oo_lev_d = np.array(origin_only.levenshtein_avg)
     np_lev_d = np.array(mrt_no_prop.levenshtein_avg)
     
-    print(full_lev_d)
+    
     ttest_res = sts.ttest_ind(full_lev_d, oo_lev_d, equal_var=False)
+    print("Full vs. Origin Only T-Test")
     print(ttest_res)
-    plot_ld(full_lev_d)
+    
+    ttest_res = sts.ttest_ind(full_lev_d, np_lev_d, equal_var=False)
+    print("Full vs. No Propagation T-Test")
+    print(ttest_res)
+
+    plot_ld(full_lev_d, oo_lev_d, np_lev_d)
 
 
 if __name__ == "__main__":
